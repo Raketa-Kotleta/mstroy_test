5acb241f93478df2c15fbad439328708c90490f4
Dmitry Yatsenko <niceikis@gmail.com>
2026-02-03 05:52:06 +0300

    Vue component realization. App.vue delete garbage code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="ag-grid-container">
-        <tree-store-table :tree-store="treeStore"> </tree-store-table>
+        <tree-store-table></tree-store-table>
     </div>
 </template>
 
@@ -8,11 +8,7 @@
 
 <script setup lang="ts">
     import TreeStoreTable from '@/components/TreeStoreTable.vue';
-    import TreeStore from './model/TreeStore';
-    import TreeStoreItem from './model/TreeStoreItem';
-    import { useDataStore } from './stores/data';
 
-    const treeStore: TreeStore<TreeStoreItem> = new TreeStore(useDataStore().data);
 </script>
 
 <style>
